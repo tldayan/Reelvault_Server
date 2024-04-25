e23146880271cdc12bb5b83b5b0dcaaf70d98ee8
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-25 23:53:47 +0400

    Added production code changes 2

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,7 +4,7 @@ const whitelist = ["https://reelvault.vercel.app","*"]
 
     const corsOptions = {
         origin: (origin,callback) => {
-            if(whitelist.includes(origin) || !origin) { // "!origin" must be removed during Production
+            if(whitelist.includes(origin)/*  || !origin */) { // "!origin" must be removed during Production
                 callback(null, true)
             } else {
                 callback(new Error("Not allowed by Cors"))
