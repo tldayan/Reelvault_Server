eb47c7539551544e3ece6231f580d27337d495c8
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-26 13:37:33 +0400

    Changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,4 +1,4 @@
-const whitelist = ["https://reelvault.vercel.app"];
+const whitelist = ["https://reelvault.vercel.app", "http://localhost:5173"];
 
 function isFaviconRequest(req) {
   return req.originalUrl && req.originalUrl.endsWith('/favicon.ico');
@@ -6,7 +6,7 @@ function isFaviconRequest(req) {
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || whitelist.includes(origin) || isFaviconRequest(req)) {
+    if (whitelist.includes(origin) || isFaviconRequest(req)) {
       // Allow requests with no origin (e.g., favicon requests) or from whitelisted origins
       callback(null, true);
     } else {
