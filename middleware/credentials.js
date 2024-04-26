33947bf1b8d281cb3d0ae2ba9dca2c69e55b8809
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-26 19:46:40 +0400

    changes

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,24 +1,21 @@
-const whitelist = ["https://reelvault-final.vercel.app"];
 
-const credentials = (req, res, next) => {
-  const origin = req.headers.origin;
 
-  if (whitelist.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+const whitelist = ["https://reelvault-final.vercel.app"]
+
+const credentials = (req,res,next) => {
+
+    const origin  = req.headers.origin
+
+    if(whitelist.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", true);
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    if (req.method === "OPTIONS") {
-      res.sendStatus(200);
+        next()
     } else {
-      next();
+        res.status(403).send("Forbidden")
     }
-  } else {
-    res.status(403).send("Forbidden");
-  }
-};
 
-module.exports = credentials;
+    
+
+}
+
+module.exports = credentials
