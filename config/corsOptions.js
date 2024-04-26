7cd671a222613e653f45a62ba939249782d55c06
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-26 13:28:40 +0400

    Changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,16 +1,20 @@
+const whitelist = ["https://reelvault.vercel.app", "http://localhost:5173"];
 
-const whitelist = ["https://reelvault.vercel.app","http://localhost:5173"]
+function isFaviconRequest(req) {
+  return req.originalUrl && req.originalUrl.endsWith('/favicon.ico');
+}
 
-    const corsOptions = {
-        origin: (origin,callback) => {
-            if(whitelist.includes(origin) || origin.endsWith('/favicon.ico')/*  || !origin */) { // "!origin" must be removed during Production
-                callback(null, true)
-            } else {
-                callback(new Error("Not allowed by Cors"))
-            }
-        },
-        credentials : true,
-        optionsSuccessStatus: 200
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin) || isFaviconRequest(req)) {
+      // Allow requests with no origin (e.g., favicon requests) or from whitelisted origins
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by Cors"));
     }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
