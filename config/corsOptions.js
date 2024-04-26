4304a7e0621350a38bb95510787cf812f8d7a6d2
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-26 12:46:42 +0400

    Changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,11 +1,11 @@
 const credentials = require("../middleware/credentials")
 
 
-const whitelist = ["https://reelvault.vercel.app","*"]
+const whitelist = ["https://reelvault.vercel.app","http://localhost:5173"]
 
     const corsOptions = {
         origin: (origin,callback) => {
-            if(whitelist.includes(origin)/*  || !origin */) { // "!origin" must be removed during Production
+            if(whitelist.includes(origin) || !origin) { // "!origin" must be removed during Production
                 callback(null, true)
             } else {
                 callback(new Error("Not allowed by Cors"))
