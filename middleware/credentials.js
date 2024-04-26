764e085eeec8e4dfbac7ff506cd2c81aeebe5b06
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-26 20:34:18 +0400

    Chnages

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -7,7 +7,7 @@ const credentials = (req,res,next) => {
     const origin  = req.headers.origin
 
     if(whitelist.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
+/*         res.header("Access-Control-Allow-Origin", origin); */
     res.header("Access-Control-Allow-Credentials", true);
         next()
     } else {
