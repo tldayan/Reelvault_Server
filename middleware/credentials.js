482491f17b52349f0874fbc15be721fe1f3ae3f5
tldayan <110288123+tldayan@users.noreply.github.com>
2024-09-27 15:55:39 +0400

    Cors Fix

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -4,21 +4,20 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
   if (whitelist.includes(origin)) {
-
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept");
 
-
+    // Handle preflight request
     if (req.method === "OPTIONS") {
       return res.sendStatus(200);  
     }
-    
-    next(); 
   } else {
-    res.status(403).send("Forbidden: CORS policy does not allow this origin.");
+    return res.status(403).send("Forbidden: CORS policy does not allow this origin.");
   }
+  
+  next();
 };
 
 module.exports = credentials;
