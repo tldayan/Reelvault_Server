435357c219e51ca05cb008e847951cf0b4ed3b86
tldayan <110288123+tldayan@users.noreply.github.com>
2024-09-29 10:41:06 +0400

    Bug fix

--- routes/mongo/incrementStytchUser.js
+++ routes/mongo/incrementStytchUser.js
@@ -1,34 +1,28 @@
-const express = require("express")
-const StytchLimit = require("../../mongo/models/StytchLimit")
-const router = express.Router()
+const express = require("express");
+const StytchLimit = require("../../mongo/models/StytchLimit");
+const router = express.Router();
 
-
-const incrementStytchUser = async(req,res) => {
-
+const incrementStytchUser = async (req, res) => {
   try {
+    let stytchLimit = await StytchLimit.findOne();
 
-    const stytchLimit = await StytchLimit.findOne()
+    if (!stytchLimit) {
+      stytchLimit = await StytchLimit.create({ limit: 0 });
+    }
 
-      if(!stytchLimit) {
-        createStytchLimit = await StytchLimit.create({limit : 0})
-      }
-      
-      if(stytchLimit.limit <= 50) {
-        stytchLimit.limit++
-        const result = await stytchLimit.save()
-        return res.sendStatus(200)
-      }
+    if (stytchLimit.limit < 50) { // Ensure it's less than 50
+      stytchLimit.limit++;
+      await stytchLimit.save();
+      return res.sendStatus(200);
+    }
 
-      return res.sendStatus(403)
-      
-
-  } catch(err) {
-    console.log(err)
-    return res.status(500).send({ message: 'Error incrementing limit', err })
+    return res.sendStatus(403); // Limit exceeded
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: 'Error incrementing limit', err });
   }
-}
+};
 
-router.route("/").post(incrementStytchUser)
+router.route("/").post(incrementStytchUser);
 
-
-module.exports = router
+module.exports = router;
