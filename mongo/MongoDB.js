a0348143718ff2e33463ad11e5e4580d78169eba
tldayan <110288123+tldayan@users.noreply.github.com>
2024-10-22 12:42:37 +0400

    Improved MongoDB Connection

--- mongo/MongoDB.js
+++ mongo/MongoDB.js
@@ -2,13 +2,23 @@ const mongoose = require("mongoose")
 const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
+
+let isConnected 
+
 const connectDB = async() => {
+
+    if(isConnected) {
+        return
+    }
+    
+
     try {
         
-        await mongoose.connect(process.env.MONGODB_URI)
-
+        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        isConnected = true
     } catch (err) {
         console.log(err.message)
+        isConnected = false
     }
 }
 
